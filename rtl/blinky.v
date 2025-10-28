d7dc078e2e9a1c34d7f83bf81fca4bc448c0597e
iwilson <isaac.wilson@lasp.colorado.edu>
2025-10-28 02:15:31 -0600

    ran blinky through various file generators

---
+++ rtl/blinky.v
@@ -0,0 +1,23 @@
+module blinky(
+    input clk,
+    input rst,
+    output [7:0] LED,
+    output [23:0] IO_LED
+);
+
+    localparam COUNTER_WIDTH = 32 + 8;
+    reg [COUNTER_WIDTH-1:0] counter;
+
+    always @(posedge clk) begin
+        if (rst == 1'b0) begin
+            counter <= 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+
+    assign LED = counter[COUNTER_WIDTH-1:COUNTER_WIDTH-8];
+    assign IO_LED = counter[COUNTER_WIDTH-9:COUNTER_WIDTH-(8 + 24)];
+
+
+endmodule
